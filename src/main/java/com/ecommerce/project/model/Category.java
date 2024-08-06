7a22b8a705a728b746952addfbbf20c7f96f72d0
Snehith1838 <lyagasnehithreddy@gmail.com>
2024-08-06 16:05:02 +0530

    ecommerce project added

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,27 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    @NotBlank
+    @Size(min = 3, message = "Category name must contain atleast 3 characters")
+    private String categoryName;
+    private String subCategory;
+
+}
